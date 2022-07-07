564eb811f78986e601f99b40762ff12da9c6a9cd
Raj Sahu <sahuraj457@gmail.com>
2022-07-08 02:58:23 +0530

    corrected files placed

--- Web Dev Simplified (current project)/store.js
+++
@@ -1,6 +0,0 @@
-var removeCartItemButtons = document.getElementsByClassName('btnDanger')
-console.log(removeCartItemButtons)
-for(var i=0; i< removeCartItemButtons.length;i++) {
-    var button = removeCartItemButtons[i]
-    button.addEventListener('click')
-}
